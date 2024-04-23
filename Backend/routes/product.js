234832572122fefecc7b01935f2ee74d8cde4df4
Shrikant Kale <40916158+shrikantkale136@users.noreply.github.com>
2024-04-22 22:23:41 -0500

    updated code

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -82,35 +82,226 @@ router.get("/getByID/:id", (req, res, next) => {
 router.patch("/update", auth.authenticate, role.checkRole, (req, res, next) => {
   let product = req.body;
 
-  let checkBackOrderQuery = "select quantity from product where id=?";
-  let query =
-    "update product set name=?, categoryID=?, description=?, price=?, quantity=? where id=?";
-  console.log("Received product data:", product);
-  console.log("SQL Query:", query);
+  let checkBackOrderQuery = "SELECT quantity FROM backorder WHERE productID=?";
+  let updateProductQuery =
+    "UPDATE product SET name=?, categoryID=?, description=?, price=?, quantity=? WHERE id=?";
+  let updateBackOrderQuery =
+    "UPDATE backorder SET quantity = ?, status=? WHERE productID=? AND status='unfinished'";
 
+  console.log("Received product data:", product);
+
+  // Check if the productID exists in backorder table
   connection.query(
-    query,
-    [
-      product.name,
-      product.categoryID,
-      product.description,
-      product.price,
-      product.quantity,
-      product.id,
-    ],
-    (err, results) => {
-      if (!err) {
-        console.log("Update Results:", results);
-        if (results.affectedRows == 0) {
-          return res.status(404).json({ message: "Product ID not found" });
-        }
-        return res
-          .status(200)
-          .json({ message: "Product updated successfully" });
-      } else {
-        console.error("Update Error:", err);
-        return res.status(500).json({ err });
+    checkBackOrderQuery,
+    [product.id],
+    (backOrderErr, backOrderResults) => {
+      if (backOrderErr) {
+        console.error("Back Order Check Error:", backOrderErr);
+        return res.status(500).json({ err: backOrderErr });
       }
+
+      connection.beginTransaction((transactionErr) => {
+        if (transactionErr) {
+          console.error("Transaction Begin Error:", transactionErr);
+          return res.status(500).json({ err: transactionErr });
+        }
+
+        connection.query(
+          updateProductQuery,
+          [
+            product.name,
+            product.categoryID,
+            product.description,
+            product.price,
+            product.quantity,
+            product.id,
+          ],
+          (productErr, productResults) => {
+            if (productErr) {
+              console.error("Update Product Error:", productErr);
+              connection.rollback(() => {
+                return res.status(500).json({ err: productErr });
+              });
+            }
+
+            if (backOrderResults.length > 0) {
+              const newQuantity = Math.floor(
+                0,
+                backOrderResults[0].quantity - product.quantity
+              );
+
+              connection.query(
+                updateBackOrderQuery,
+                [
+                  newQuantity,
+                  newQuantity <= 0 ? "finished" : "unfinished",
+                  product.id,
+                ],
+                (backOrderUpdateErr, backOrderUpdateResults) => {
+                  if (backOrderUpdateErr) {
+                    console.error(
+                      "Update Back Order Error:",
+                      backOrderUpdateErr
+                    );
+                    connection.rollback(() => {
+                      return res.status(500).json({ err: backOrderUpdateErr });
+                    });
+                  }
+
+                  // Commit
+                  connection.commit((commitErr) => {
+                    if (commitErr) {
+                      console.error("Transaction Commit Error:", commitErr);
+                      connection.rollback(() => {
+                        return res.status(500).json({ err: commitErr });
+                      });
+                    }
+
+                    return res
+                      .status(200)
+                      .json({ message: "Product updated successfully" });
+                  });
+                }
+              );
+            } else {
+              // Commit the transaction if all queries succeed
+              connection.commit((commitErr) => {
+                if (commitErr) {
+                  console.error("Transaction Commit Error:", commitErr);
+                  connection.rollback(() => {
+                    return res.status(500).json({ err: commitErr });
+                  });
+                }
+
+                return res
+                  .status(200)
+                  .json({ message: "Product updated successfully" });
+              });
+            }
+          }
+        );
+      });
+    }
+  );
+});
+
+router.patch("/update", auth.authenticate, role.checkRole, (req, res, next) => {
+  let product = req.body;
+
+  let checkBackOrderQuery = "SELECT quantity FROM backorder WHERE productID=?";
+  let updateProductQuery =
+    "UPDATE product SET name=?, categoryID=?, description=?, price=?, quantity=? WHERE id=?";
+  let updateBackOrderQuery =
+    "UPDATE backorder SET quantity = quantity - ?, status=? WHERE productID=? AND status='unfinished'";
+
+  console.log("Received product data:", product);
+
+  // Check if the productID exists in backorder table
+  connection.query(
+    checkBackOrderQuery,
+    [product.id],
+    (backOrderErr, backOrderResults) => {
+      if (backOrderErr) {
+        console.error("Back Order Check Error:", backOrderErr);
+        return res.status(500).json({ err: backOrderErr });
+      }
+
+      connection.beginTransaction((transactionErr) => {
+        if (transactionErr) {
+          console.error("Transaction Begin Error:", transactionErr);
+          return res.status(500).json({ err: transactionErr });
+        }
+
+        // Update product table
+        connection.query(
+          updateProductQuery,
+          [
+            product.name,
+            product.categoryID,
+            product.description,
+            product.price,
+            product.quantity,
+            product.id,
+          ],
+          (productErr, productResults) => {
+            if (productErr) {
+              console.error("Update Product Error:", productErr);
+              connection.rollback(() => {
+                return res.status(500).json({ err: productErr });
+              });
+            }
+
+            // Check if the productID exists in backorder and update backorder quantity and status
+            if (backOrderResults.length > 0) {
+              const newQuantity =
+                backOrderResults[0].quantity - product.quantity;
+
+              // Update backorder quantity and status if new quantity is 0 or less
+              if (newQuantity <= 0) {
+                connection.query(
+                  updateBackOrderQuery,
+                  [product.quantity, "complete", product.id],
+                  (backOrderUpdateErr, backOrderUpdateResults) => {
+                    if (backOrderUpdateErr) {
+                      console.error(
+                        "Update Back Order Error:",
+                        backOrderUpdateErr
+                      );
+                      connection.rollback(() => {
+                        return res
+                          .status(500)
+                          .json({ err: backOrderUpdateErr });
+                      });
+                    }
+
+                    // Commit the transaction if all queries succeed
+                    connection.commit((commitErr) => {
+                      if (commitErr) {
+                        console.error("Transaction Commit Error:", commitErr);
+                        connection.rollback(() => {
+                          return res.status(500).json({ err: commitErr });
+                        });
+                      }
+
+                      return res
+                        .status(200)
+                        .json({ message: "Product updated successfully" });
+                    });
+                  }
+                );
+              } else {
+                // Commit the transaction if all queries succeed
+                connection.commit((commitErr) => {
+                  if (commitErr) {
+                    console.error("Transaction Commit Error:", commitErr);
+                    connection.rollback(() => {
+                      return res.status(500).json({ err: commitErr });
+                    });
+                  }
+
+                  return res
+                    .status(200)
+                    .json({ message: "Product updated successfully" });
+                });
+              }
+            } else {
+              // Commit the transaction if all queries succeed
+              connection.commit((commitErr) => {
+                if (commitErr) {
+                  console.error("Transaction Commit Error:", commitErr);
+                  connection.rollback(() => {
+                    return res.status(500).json({ err: commitErr });
+                  });
+                }
+
+                return res
+                  .status(200)
+                  .json({ message: "Product updated successfully" });
+              });
+            }
+          }
+        );
+      });
     }
   );
 });
